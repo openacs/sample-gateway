80072222da4404952b248b99b33ee0186d7bcd70
Deds Castillo <deds@i-manila.com.ph>
2005-01-27 02:45:54 +0000

    ported to PG so that it can be used for ecommerce testing purposes

---
+++ sql/postgresql/sample-gateway-drop.sql
@@ -0,0 +1,12 @@
+-- 
+-- packages/sample-gateway/sql/postgresql/sample-gateway-drop.sql
+-- 
+-- @author Deds Castillo (deds@i-manila.com.ph)
+-- @creation-date 2005-01-24
+-- @arch-tag: b0d9e356-8669-47cd-b697-24042bd5c26d
+-- @cvs-id $Id$
+--
+
+drop table sample_gateway_result_log;
+
+\i sample-gateway-sc-drop.sql
